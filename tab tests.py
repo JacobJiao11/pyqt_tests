67aab0972102351e5ebc357e50160042e9b927f4
JacobJiao11 <jjiao2@sfsu.edu>
2026-03-08 21:56:01 -0700

    readded table widget

--- tab tests.py
+++ tab tests.py
@@ -14,7 +14,7 @@ from PyQt6.QtWidgets import (
 from PyQt6.QtGui import QIcon
 from PyQt6.QtCore import pyqtSlot
 import csv
-from table_tests import MyTableWidget
+from table_tests import MyTableWidget as dataTable
 
 class App(QMainWindow):
 
@@ -33,6 +33,40 @@ class App(QMainWindow):
         
         self.show()
     
+class MyTableWidget(QWidget):
+    
+    def __init__(self, parent):
+        super(QWidget, self).__init__(parent)
+        self.layout = QVBoxLayout(self)
+        
+        # Initialize tab screen
+        self.tabs = QTabWidget()
+        self.tab1 = QWidget()
+        self.tab2 = QWidget()
+        self.tabs.resize(300,200)
+        
+        # Add tabs
+        self.tabs.addTab(self.tab1,"Tab 1")
+        self.tabs.addTab(self.tab2,"Tab 2")
+        
+        # Create first tab
+        self.tab1.layout = QVBoxLayout(self)
+        self.pushButton1 = QPushButton("PyQt5 button")
+        self.table = dataTable() 
+        self.tab1.layout.addWidget(self.pushButton1)
+        self.tab1.layout.addWidget(self.table)
+        self.tab1.setLayout(self.tab1.layout)
+        
+        # Add tabs to widget
+        self.layout.addWidget(self.tabs)
+        self.setLayout(self.layout)
+        
+    @pyqtSlot()
+    def on_click(self):
+        print("\n")
+        for currentQTableWidgetItem in self.tableWidget.selectedItems():
+            print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
